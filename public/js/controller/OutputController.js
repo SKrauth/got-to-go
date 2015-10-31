b2543d1194b677bc76551c2fdaf3bd67e623da7c
[Sean Krauth] <[spkrauth@gmail.com]>
2015-10-30 22:48:16 -0400

    mostly working output, still struggling with controller running function after page load.

--- public/js/controller/OutputController.js
+++ public/js/controller/OutputController.js
@@ -1,13 +1,14 @@
 app.controller("OutputController", function($http, $scope){
-    $scope.rrRating = {};
+    $scope.rrRating = [];
     
-    var.getRating = function(){
+    var getRating = function(){
         $http.get("/api/restrooms")
             .then(function(response){
                 $scope.rrRating = response.data;
+                console.log(response.data);
             });
     };
     
     getRating();
-    
+    console.log($scope.rrRating);
 });
